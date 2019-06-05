c3c0821ce8469f7bf108cd48809164c034af47f3
xiewenlong <xiewenlong1202@gmail.com>
2019-06-05 20:49:54 +0800

    add list and detail views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -31,3 +32,8 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manager
     published = PublishedManager() # Our custom manager
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+            self.publish.strftime('%m'), self.publish.strftime('%d'),
+            self.slug])
